2b26acf22adb6c5e5a6e89bc9ba9a6961a7549be
Mateo Rodriguez <mdavidmontero6@gmail.com>
2024-12-28 01:07:44 -0500

    Agregando opciones de visualizacion para solo admin y user

--- src/components/shared/AdminSidebar.tsx
+++ src/components/shared/AdminSidebar.tsx
@@ -4,56 +4,87 @@ import Navegacion from "./Navegacion";
 
 export default function AdminSidebar() {
   const navigation = [
-    { url: "/products", text: "Productos", blank: false, admin: false },
-    { url: "/reports", text: "Reporte Diario", blank: false, admin: false },
+    {
+      url: "/products",
+      text: "Productos",
+      blank: false,
+      allowedRoles: ["USER", "ADMIN"],
+    },
+    {
+      url: "/reports",
+      text: "Reporte Diario",
+      blank: false,
+      allowedRoles: ["USER"],
+    },
     {
       url: "/reports-dias/vendedora",
       text: "Reporte Diario vendedora",
       blank: false,
-      admin: false,
+      allowedRoles: ["USER", "ADMIN"],
     },
-    { url: "/report-mes", text: "Reporte Mes", blank: false, admin: true },
-    { url: "/report-anual", text: "Reporte Anual", blank: false, admin: true },
+    {
+      url: "/report-mes",
+      text: "Reporte Mes",
+      blank: false,
+      allowedRoles: ["ADMIN"],
+    },
+    {
+      url: "/report-anual",
+      text: "Reporte Anual",
+      blank: false,
+      allowedRoles: ["ADMIN"],
+    },
     {
       url: "/ventas/conosencillo",
       text: "Ver Local",
       blank: true,
-      admin: false,
+      allowedRoles: ["USER"],
     },
-    { url: "/report-caja", text: "Reportes Caja", blank: false, admin: true },
-    { url: "/profile", text: "Perfil", blank: false, admin: false },
-    { url: "/points", text: "Puntos", blank: false, admin: true },
-    { url: "/cash-register", text: "Caja", blank: false, admin: false },
+    {
+      url: "/report-caja",
+      text: "Reportes Caja",
+      blank: false,
+      allowedRoles: ["ADMIN"],
+    },
+    {
+      url: "/profile",
+      text: "Perfil",
+      blank: false,
+      allowedRoles: ["USER", "ADMIN"],
+    },
+    { url: "/points", text: "Puntos", blank: false, allowedRoles: ["ADMIN"] },
+    {
+      url: "/cash-register",
+      text: "Caja",
+      blank: false,
+      allowedRoles: ["USER"],
+    },
   ];
 
   const user = userAuthStore((state) => state.user);
 
-  const filteredNavigation = navigation.filter((nav) => {
-    if (user?.role === "ADMIN") return true;
-    if (user?.role === "USER") return !nav.admin;
-    return false;
-  });
+  const filteredNavigation = navigation.filter((nav) =>
+    nav.allowedRoles.includes(user!.role)
+  );
 
   return (
-    <>
-      <aside className="flex flex-col bg-white md:w-72 md:h-screen">
-        {/* Parte fija: imagen y nombre del usuario */}
-        <div className="flex flex-col items-center flex-none p-5">
-          <FotoPerfil />
-          <p className="mt-5 font-bold text-center">{user?.name}</p>
-        </div>
-        {/* Parte con scroll: navegación */}
-        <div className="flex-1 space-y-3 overflow-y-auto">
-          <p className="mt-5 text-sm font-bold text-center text-gray-600 uppercase">
-            Navegación
-          </p>
-          <nav className="flex flex-col">
-            {filteredNavigation.map((nav) => (
-              <Navegacion key={nav.url} link={nav} />
-            ))}
-          </nav>
-        </div>
-      </aside>
-    </>
+    <aside className="flex flex-col bg-white md:w-72 md:h-screen">
+      {/* Parte fija: imagen y nombre del usuario */}
+      <div className="flex flex-col items-center flex-none p-5">
+        <FotoPerfil />
+        <p className="mt-5 font-bold text-center">{user?.name}</p>
+      </div>
+      {/* Parte con scroll: navegación */}
+      <div className="flex-1 space-y-3 overflow-y-auto">
+        <p className="mt-5 text-sm font-bold text-center text-gray-600 uppercase">
+          Navegación
+        </p>
+        <nav className="flex flex-col">
+          {filteredNavigation.map((nav) => (
+            <Navegacion key={nav.url} link={nav} />
+          ))}
+        </nav>
+      </div>
+    </aside>
   );
 }
