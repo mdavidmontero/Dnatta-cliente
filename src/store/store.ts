435d3adab05a26f56686d1a566c836b8185116fb
Mateo Rodriguez <mdavidmontero6@gmail.com>
2025-03-04 10:05:06 -0500

    Agregando middleware de persistencia al carrito

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,6 +1,7 @@
 import { create } from "zustand";
 import { OrderItem, ProductI } from "../types";
 import { openDrawerCash } from "@/actions/ventas.actions";
+import { devtools, persist } from "zustand/middleware";
 
 interface Store {
   order: OrderItem[];
@@ -13,81 +14,90 @@ interface Store {
   openCashDrawer: () => Promise<void>;
 }
 
-export const useStore = create<Store>((set, get) => ({
-  order: [],
+export const useStore = create<Store>()(
+  devtools(
+    persist(
+      (set, get) => ({
+        order: [],
 
-  addToOrder: (product) => {
-    const { ...data } = product;
-    let order: OrderItem[] = [];
-    if (get().order.find((item) => item.id === product.id)) {
-      order = get().order.map((item) =>
-        item.id === product.id
-          ? {
-              ...item,
-              quantity: item.quantity + 1,
-              subtotal: item.price * (item.quantity + 1),
-            }
-          : item
-      );
-    } else {
-      order = [
-        ...get().order,
-        {
-          ...data,
-          quantity: 1,
-          subtotal: 1 * product.price,
-        },
-      ];
-    }
-    set(() => ({
-      order,
-    }));
-  },
-  increaseQuantity: (id) => {
-    set((state) => ({
-      order: state.order.map((item) =>
-        item.id === id
-          ? {
-              ...item,
-              quantity: item.quantity + 1,
-              subtotal: item.price * (item.quantity + 1),
-            }
-          : item
-      ),
-    }));
-  },
-  decreaseQuantity: (id) => {
-    const order = get().order.map((item) =>
-      item.id === id
-        ? {
-            ...item,
-            quantity: item.quantity - 1,
-            subtotal: item.price * (item.quantity - 1),
+        addToOrder: (product) => {
+          const { ...data } = product;
+          let order: OrderItem[] = [];
+          if (get().order.find((item) => item.id === product.id)) {
+            order = get().order.map((item) =>
+              item.id === product.id
+                ? {
+                    ...item,
+                    quantity: item.quantity + 1,
+                    subtotal: item.price * (item.quantity + 1),
+                  }
+                : item
+            );
+          } else {
+            order = [
+              ...get().order,
+              {
+                ...data,
+                quantity: 1,
+                subtotal: 1 * product.price,
+              },
+            ];
           }
-        : item
-    );
+          set(() => ({
+            order,
+          }));
+        },
+        increaseQuantity: (id) => {
+          set((state) => ({
+            order: state.order.map((item) =>
+              item.id === id
+                ? {
+                    ...item,
+                    quantity: item.quantity + 1,
+                    subtotal: item.price * (item.quantity + 1),
+                  }
+                : item
+            ),
+          }));
+        },
+        decreaseQuantity: (id) => {
+          const order = get().order.map((item) =>
+            item.id === id
+              ? {
+                  ...item,
+                  quantity: item.quantity - 1,
+                  subtotal: item.price * (item.quantity - 1),
+                }
+              : item
+          );
 
-    set(() => ({
-      order,
-    }));
-  },
-  removeItem: (id) => {
-    set((state) => ({
-      order: state.order.filter((item) => item.id !== id),
-    }));
-  },
+          set(() => ({
+            order,
+          }));
+        },
+        removeItem: (id) => {
+          set((state) => ({
+            order: state.order.filter((item) => item.id !== id),
+          }));
+        },
 
-  filtro: (value) => {
-    set((state) => ({
-      order: state.order.filter((item) => item.name.includes(value)),
-    }));
-  },
-  clearOrder: () => {
-    set(() => ({
-      order: [],
-    }));
-  },
-  openCashDrawer: async () => {
-    await openDrawerCash();
-  },
-}));
+        filtro: (value) => {
+          set((state) => ({
+            order: state.order.filter((item) => item.name.includes(value)),
+          }));
+        },
+        clearOrder: () => {
+          set(() => ({
+            order: [],
+          }));
+        },
+        openCashDrawer: async () => {
+          await openDrawerCash();
+        },
+      }),
+      {
+        name: "pedido-store",
+      }
+    )
+  )
+);
